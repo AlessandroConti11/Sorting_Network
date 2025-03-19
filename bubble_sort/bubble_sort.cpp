ec957ac6ccacffe1995146fb3fa69181f363cf3b
alessandro-conti <alessandro11.conti@mail.polimi.it>
2025-03-19 19:55:59 +0100

    sorting_network - bubble_sort - master: implemented the bubble sort algorithm

---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,19 @@
+#include "bubble_sort.h"
+
+
+/**
+ * Bubble Sort algorithm.
+ *
+ * @param array the unsorted array.
+ */
+void Bubble_Sort::bubble_Sort(vector<int>& array) {
+    int n = (int) array.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1])
+
+                swap(array[j], array[j + 1]);
+        }
+    }
+}
