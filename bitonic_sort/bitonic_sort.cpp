0d35534de3ccc79abefbdaad0438c50fe26d3531
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2025-04-19 15:37:57 +0200

    Sorting_Network - bitonic_sort - master: updated bitonic_sort so that it is parallel

--- bitonic_sort/bitonic_sort.cpp
+++ bitonic_sort/bitonic_sort.cpp
@@ -9,7 +9,7 @@
  * @param array the unsorted array.
  */
 void Bitonic_Sort::bitonic_sort(std::vector<int>& array) {
-    sort_bitonic(array, 0, static_cast<int>(array.size()), Bitonic_Sort::ASCENDING);
+    sort_bitonic(array, 0, static_cast<int>(array.size()), ASCENDING);
 }
 
 
@@ -21,12 +21,24 @@ void Bitonic_Sort::bitonic_sort(std::vector<int>& array) {
  * @param finish_position the finishing position.
  * @param direction the sorting direction.
  */
-void Bitonic_Sort::sort_bitonic(vector<int> &array, const int start_position, const int finish_position, const bool direction) {
+void Bitonic_Sort::sort_bitonic(std::vector<int> &array, const int start_position, const int finish_position, const bool direction) {
     if(finish_position > 1) {
+        ///The new final position.
         const int m = finish_position / 2;
 
-        sort_bitonic(array, start_position, m, Bitonic_Sort::ASCENDING);
-        sort_bitonic(array, start_position + m, m, Bitonic_Sort::DESCENDING);
+
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                sort_bitonic(array, start_position, m, ASCENDING);
+            }
+
+            #pragma omp section
+            {
+                sort_bitonic(array, start_position + m, m, DESCENDING);
+            }
+        }
 
         merge_bitonic(array, start_position, finish_position, direction);
     }
@@ -40,12 +52,17 @@ void Bitonic_Sort::sort_bitonic(vector<int> &array, const int start_position, co
  * @param finish_position the finishing position.
  * @param direction the sorting direction.
  */
-void Bitonic_Sort::merge_bitonic(vector<int> &array, const int start_position, const int finish_position, const bool direction) {
+void Bitonic_Sort::merge_bitonic(std::vector<int> &array, const int start_position, const int finish_position, const bool direction) {
     if(finish_position > 1) {
+        ///The new final position.
         const int m = finish_position / 2;
 
-        for (int i = start_position; i < start_position + m; ++i) {
-            compare_and_swap(array, i, i + m, direction);
+
+        #pragma omp parallel for
+        {
+            for (int i = start_position; i < start_position + m; ++i) {
+                compare_and_swap(array, i, i + m, direction);
+            }
         }
 
         merge_bitonic(array, start_position, m, direction);
--- bitonic_sort/bitonic_sort.h
+++ bitonic_sort/bitonic_sort.h
@@ -3,6 +3,8 @@
 
 
 #include <vector>
+#include <omp.h>
+
 using namespace std;
 
 
